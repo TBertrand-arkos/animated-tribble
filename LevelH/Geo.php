1e167fe0a5db3850ec2fbcddaf58ecbeb06966cf
TBertrand_arkos <bertrand.tarascon@epita.fr>
2022-10-26 21:33:02 +0200

    [FIX] LevelH

--- LevelH/Geo.php
+++ LevelH/Geo.php
@@ -16,21 +16,31 @@ class Geo
         $srcCity = $myCities->getCityInfoById($id);
         $closestDistance = PHP_INT_MAX;
         $closestCity = $srcCity;
+        $rlo1 = deg2rad($srcCity['lat']);
+        $rla1 = deg2rad($srcCity['long']);
         $cosDeg2RadLat1 = cos(deg2rad($srcCity['lat']));
+        $earth_radius = 6378137; // Earth Radius is 6378.137 km
 
-        foreach ($cities as $dstCity) {
-            if ($dstCity['id'] === $srcCity['id']){
+        foreach($cities as $dstCity) {
+
+            if($dstCity['id'] === $srcCity['id']) {
+                continue;
+            }
+
+            if($dstCity['lat'] >= $srcCity['lat']+1 || $dstCity['lat'] <= $srcCity['lat']-1 &&
+            $dstCity['long'] >= $srcCity['long']+1 || $dstCity['long'] <= $srcCity['long']-1) {
                 continue;
             }
 
             $distance = $this->computeDistance(
-                $srcCity['lat'],
-                $srcCity['long'],
+                $rlo1,
+                $rla1,
                 $dstCity['lat'],
                 $dstCity['long'],
-                $cosDeg2RadLat1
+                $cosDeg2RadLat1,
+                $earth_radius
             );
-
+            
             if ($closestDistance > $distance) {
                 $closestDistance = $distance;
                 $closestCity = $dstCity;
@@ -44,18 +54,15 @@ class Geo
     /**
      * Give the distance in meter between two points (in kilometer)
      *
-     * @param $lat1
-     * @param $lng1
+     * @param $rlo1
+     * @param $rla2
      * @param $lat2
      * @param $lng2
      * @return int
      */
 
-    private function computeDistance($lat1, $lng1, $lat2, $lng2, $cosDeg2RadLat1){
-
-        $earth_radius = 6378137; // Earth Radius is 6378.137 km
-        $rlo1 = deg2rad($lng1);
-        $rla1 = deg2rad($lat1);
+    private function computeDistance($rla1, $rlo1, $lat2, $lng2, $cosDeg2RadLat1, $earth_radius){
+        
         $rlo2 = deg2rad($lng2);
         $rla2 = deg2rad($lat2);
         $dlo = ($rlo2 - $rlo1) / 2;
